3da9beb0221beb2d4c1bbf10648d18f9614e0526
thaontt35_onemount <thaontt35@onemount.com>
2021-08-12 14:04:50 +0700

    sort array

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,23 +1,37 @@
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(arr[0]);
+        System.out.print("n = ");
+        int n = sc.nextInt();
 
-//        for (int i = 0; i < arr.length; i++){
-//            System.out.format("%d,", arr[i]);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.format("arr[%d], ", i);
+            arr[i] = sc.nextInt();
+        }
+
+        printArray(arr);
+        System.out.format("Sum = %d\n", sumElements(arr));
+
+        sortArray(arr);
+        printArray(arr);
+
+//        int[] arr = {1, 2, 3, 4};
+//        System.out.println(arr[0]);
+
+
+//        for (int element : arr) {
+//            System.out.format("%d,", element);
 //        }
 //        System.out.println();
 
-        for (int element : arr) {
-            System.out.format("%d,", element);
-        }
-        System.out.println();
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.format("%d,", arr[i]);
-        }
-        System.out.println();
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            System.out.format("%d,", arr[i]);
+//        }
+//        System.out.println();
 
 //        int j = 0;
 //        while ( j < arr.length){
@@ -25,5 +39,33 @@ public class Array {
 //            j++;
 //        }
 //        System.out.println();
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.format("%d,", arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static int sumElements(int[] arr) {
+        int ret = 0;
+        for (int ele : arr) {
+            ret += ele;
+        }
+        return ret;
+    }
+
+    //
+    public static void sortArrayAsc(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
     }
 }
