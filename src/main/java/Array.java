94a9ec7f36f4afd7e970569f346e6bd104427077
thaontt35_onemount <thaontt35@onemount.com>
2021-08-13 10:01:47 +0700

    new array

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -2,19 +2,37 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.print("n = ");
+//        int n = sc.nextInt();
+//
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            System.out.format("arr[%d], ", i);
+//            arr[i] = sc.nextInt();
+//        }
 
-        System.out.print("n = ");
-        int n = sc.nextInt();
+        int[] arr = generateArray(20,0,100);
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.format("arr[%d], ", i);
-            arr[i] = sc.nextInt();
-        }
+        int maxIdx = findMaxIndex(arr);
+        System.out.format("MAX =%d");
 
         printArray(arr);
         System.out.format("Sum = %d\n", sumElements(arr));
+
+        System.out.format("MAX =%d\n",findMax(arr));
+        System.out.format("MIN =%d\n",findMin(arr));
+
+        int firstNegIdx = findFirstNagativeNumber(arr);
+        if (firstNegIdx >=0){
+            System.out.format("FirstNegative = %d", Index= %d\n, arr[firstNegIdx],firstNegIdx);
+        }else System.out.println(Khong co phan tu am trong day);
+
+        int lastPostIdx = findLastPositiveIndex(arr);
+        if (lastPostIdx >=0){
+            System.out.format("LastPositive =%d", Index =%d\n, arr[lastPostIdx], lastPostIdx);
+        }else System.out.println(Khong co phan tu duong trong day);
 
         sortArrayAsc(arr);
         printArray(arr);
@@ -40,6 +58,7 @@ public class Array {
 //        }
 //        System.out.println();
     }
+
 
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -67,4 +86,79 @@ public class Array {
             }
         }
     }
+
+//    public static int[] generateArray(int n, int min, int max){
+//        int[] ret = new int[n];
+//        for (int i=0; i<ret.length; i++){
+//            ret[i]= (int)Math.floor(Math.radom()*100);
+//        }
+//        return  ret;
+//
+//    }
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int findMaxIndex(int[] arr) {
+        int idx = 0;
+
+        for (int i = 1; i< arr.length;i++){
+            if (arr[i] > arr[idx]) {
+                idx = i;
+            }
+        }
+        return idx;
+    }
+
+    public  static int findFirstNagativeNumber(int[] arr){
+        for (int i =0;i<arr.length; i++){
+            if (arr[i]<0){
+                return i;
+            }
+        }
+        return -1; // khong co phan tu am
+    }
+
+    public  static int findLastPositiveIndex(int[] arr){
+        for (int i =arr.length-1; i>=0;i++){
+            if (arr[i]>0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int findMinPositiveIndex(int[] arr){
+        int lastPosIdx = findLastPositiveIndex(arr);
+        if (lastPosIdx <0){
+            return -1;
+        }
+        int minPostIdx = lastPosIdx;
+        for (int i =0; i<arr.length; i++){
+            if(arr[i]>0 && arr[i] <arr[minPostIdx]){
+                minPostIdx=i;
+            }
+        }
+        return minPostIdx;
+    }
+
 }
+
